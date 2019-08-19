aa7ae1e71672734ccc5d6f19e4aec454ca6aafed
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-08-19 16:59:28 -0500

    Stretch

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,10 @@
-// Update with your config settings.
-
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    },     
-    useNullAsDefault: true,
-    migrations: {
-      directory: './db/migrations'
-    },
-    seeds:{
-      directory: './db/seeds'
-    }
-
- 
-  }}
- 
+	development: {
+		client: 'sqlite3',
+		connection: {
+      filename: './auth.db3',
+      directory: './data'
+		},
+		useNullAsDefault: true
+	}
+};
